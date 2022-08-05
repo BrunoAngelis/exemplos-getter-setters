ecea75d34e9300776c2297d1ca2ad45c9878508b
BrunoAngelis <bng.8@outlookcom>
2022-08-05 09:16:25 -0300

    Adiconando exemplos de construtores

---
+++ src/SistemaCadastro.java
@@ -0,0 +1,10 @@
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        Pessoa bruno = new Pessoa("Bruno Garcia de Angelis","12345678910");
+
+        bruno.setEndereco("Rua Benedito Rosa");
+
+        System.out.println(bruno.getNome()+" - "+bruno.getCpf());
+
+    }
+}
